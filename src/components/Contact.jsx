619eb9acc7ad540429f9a61581955cc61032d328
yash chauhan <yash.chauhan.ug20@nsut.ac.in>
2024-06-22 17:33:26 +0530

    third commit resolving errors

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -64,7 +64,7 @@ export const Contact = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <h2 className=" capitalize">Get In Touch or You can contact me on my socials</h2>
-                  <form onSubmit={handleSubmit} method="POST" action={process.env.REACT_EMAIL_KEY}>
+                  <form  method="POST" action={process.env.REACT_EMAIL_KEY}>
                     <Row>
                       <Col size={12} sm={6} className="px-1">
                         <input type="text" value={formDetails.firstName} placeholder="Name" required onChange={(e) => onFormUpdate('firstName', e.target.value)} />
